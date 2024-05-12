87082b2baa328f92c6551b78e247cdcf426fac33
Antonio Bonifacio <antonio.bonifacio@telepass.com>
2024-05-12 12:27:15 +0200

    feat: add swagger

--- onboarding-inbound/src/main/java/org/acme/onboarding/inbound/api/v1/upload/DocumentUploadResourceController.java
+++ onboarding-inbound/src/main/java/org/acme/onboarding/inbound/api/v1/upload/DocumentUploadResourceController.java
@@ -10,6 +10,15 @@ import org.acme.onboarding.domain.model.user.Username;
 import org.acme.onboarding.inbound.annotation.InboundDelegate;
 import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.multipart.FileUpload;
 
@@ -17,9 +26,18 @@ import java.util.concurrent.CompletionStage;
 import java.util.logging.Logger;
 
 import static java.util.concurrent.CompletableFuture.failedStage;
+import static org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType.HTTP;
 
 @RequestScoped
 @Path("/api/v1/documents")
+@Tag(name = "upload", description = "Upload ID document")
+@SecurityRequirement(name = "Bearer token")
+@SecurityScheme(
+        description = "Header name: Authorization",
+        securitySchemeName = "Bearer token",
+        scheme = "bearer",
+        bearerFormat = "bearer",
+        type = HTTP)
 public class DocumentUploadResourceController {
     private final Logger log = Logger.getLogger(getClass().getName());
     private final JsonWebToken jsonWebToken;
@@ -36,7 +54,44 @@ public class DocumentUploadResourceController {
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
     @PermissionsAllowed("email")
-    public CompletionStage<Void> upload(@RestForm("image") FileUpload file) {
+    @Operation(operationId = "uploadDocument")
+    @APIResponse(
+            responseCode = "204",
+            name = "204",
+            description = "Document uploaded",
+            content = {@Content(mediaType = "application/json")})
+    @APIResponse(
+            responseCode = "401",
+            name = "401",
+            description = "Unauthorized",
+            content = {@Content(mediaType = "application/json")})
+    @APIResponse(
+            responseCode = "403",
+            name = "403",
+            description = "Forbidden",
+            content = {@Content(mediaType = "application/json")})
+    @APIResponse(
+            responseCode = "404",
+            name = "404",
+            description = "Bad Request",
+            content = {@Content(mediaType = "application/json")})
+    @APIResponse(
+            responseCode = "500",
+            name = "500",
+            description = "Internal error",
+            content = {@Content(mediaType = "application/json")})
+    public CompletionStage<Void> upload(
+            @RequestBody(
+                            content =
+                                    @Content(
+                                            mediaType = "multipart/form-data",
+                                            schema =
+                                                    @Schema(
+                                                            type = SchemaType.OBJECT,
+                                                            description =
+                                                                    "multipart form. multiple files can be passed")))
+                    @RestForm("image")
+                    FileUpload file) {
         if (jsonWebToken.getClaim(Claims.email_verified.name())) {
             if (file == null) {
                 return failedStage(new BadRequestException("Missing file"));
